bd8dcce3ea6f4a5872421448fc65e87245a6fd0e
Lewisbrown44 <lewisbrown44@outlook.com>
2025-10-21 14:36:15 +0000

    basic project structure

---
+++ itreporting/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request): 
+     return render(request, 'itreporting/home.html', {'title': 'Welcome'})
+
+ 
+def about(request): 
+     return render(request, 'itreporting/about.html', {'title': 'Welcome'})
+
+def contact(request):
+     return render(request, 'itreporting/contact.html', {'title': 'Welcome'})
+
+# Create your views here.
